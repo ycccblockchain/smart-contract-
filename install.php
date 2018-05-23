003e35ae1df630aef98d6fed4d020a9d36cc7c1f
ycccblockchain <39395283+ycccblockchain@users.noreply.github.com>
2018-05-23 10:24:59 +0800

    Update install.php

--- install.php
+++ install.php
@@ -369,3 +369,216 @@ function check(obj)
 <?php
         } ?>
 	<div class="botBorder">
+		<p><?php echo $i_message['install_founder_name'], ': ', $email?></p>
+		<p><?php echo $i_message['install_founder_password'], ': ', $password; ?></p>
+   	</div>
+<?php
+        } ?>
+	<div class="botBorder">
+
+<?php
+if (!$quit) {
+
+$randkey = uniqid(rand());
+            $fp = fopen(THINKSNS_ROOT.'/config/'.$thinksns_config_file, 'wb');
+            $configfilecontent = <<<EOT
+<?php
+if (!defined('SITE_PATH')) exit();
+return array(
+
+	'DB_TYPE'       => 'mysql',
+	
+	'DB_HOST'       => '$db_host',
+	'DB_NAME'       => '$db_name',
+	'DB_USER'       => '$db_username',
+	'DB_PWD'        => '$db_password',
+
+	'DB_PORT'       => 3306,
+	'DB_PREFIX'     => '$db_prefix',
+	'DB_CHARSET'    => 'utf8',
+	'SECURE_CODE'   => '$randkey',
+	'COOKIE_PREFIX' => 'TS4_',
+);
+EOT;
+            $configfilecontent = str_replace('SECURE_TEST', 'SECURE'.rand(10000, 20000), $configfilecontent);
+            chmod(THINKSNS_ROOT.'/config/'.$thinksns_config_file, 0777);
+            $result_1 = fwrite($fp, trim($configfilecontent));
+            @fclose($fp);
+
+            if ($result_1 && file_exists(THINKSNS_ROOT.'/config/'.$thinksns_config_file)) {
+                ?>
+	<p><?php echo $i_message['config_log_success']; ?></p>
+<?php
+            } else {
+                ?>
+	<p><?php echo $i_message['config_read_failed'];
+                $quit = true; ?></p>
+<?php
+            }
+        } ?>
+	</div>
+	<div class="center">
+		<form method="post" action="install.php?v=5">
+		<input type="button" class="submit" name="prev" value="<?php echo $i_message['install_prev']; ?>" onClick="history.go(-1)">&nbsp;
+		<input type="submit" class="submit" name="next" value="<?php echo $i_message['install_next']; ?>" <?php echo $allownext; ?> >
+		</form>
+	</div>
+<?php
+    } elseif ($v == '5') {
+        $db_config = $_SESSION['config_file_content'];
+
+        if (!$db_config['db_host'] && !$db_config['db_name']) {
+            $msg .= '<p>error:'.$i_message['configure_read_failed'].'</p>';
+            $quit = true;
+        } else {
+            mysql_connect($db_config['db_host'], $db_config['db_username'], $db_config['db_password']);
+            $sqlv = mysql_get_server_info();
+            if (version_compare($sqlv, '5.0.0', '<')) {
+                $msg .= '<p>error:'.$i_message['mysql_version_402'].'</p>';
+                $quit = true;
+            } else {
+                $db_charset = $db_config['db_charset'];
+                $db_charset = (strpos($db_charset, '-') === false) ? $db_charset : str_replace('-', '', $db_charset);
+
+                mysql_query(" CREATE DATABASE IF NOT EXISTS `{$db_config['db_name']}` DEFAULT CHARACTER SET $db_charset ");
+
+                if (mysql_errno()) {
+                    $errormsg = mysql_error();
+                    $msg .= '<p>error:'.($errormsg ? $errormsg : $i_message['database_errno']).'</p>';
+                    $quit = true;
+                } else {
+                    mysql_select_db($db_config['db_name']);
+                }
+
+            $re = mysql_query("SELECT COUNT(1) FROM {$db_config['db_prefix']}user");
+                $link = @mysql_fetch_row($re);
+
+                if (intval($link[0]) > 0) {
+                    $thinksns_rebuild = true;
+                    $msg .= '<p>error:'.$i_message['thinksns_rebuild'].'</p>';
+                    $alert = ' onclick="return confirm(\''.$i_message['thinksns_rebuild'].'\');"';
+                }
+            }
+        }
+
+        if ($quit) {
+            $allownext = 'disabled="disabled"'; ?>
+<?php
+    echo $msg;
+        } else {
+            ?>
+<div class="botBorder">
+<?php
+if ($thinksns_rebuild) {
+                ?>
+<p style="color:red;font-size:16px;"><?php echo $i_message['thinksns_rebuild']; ?></p>
+<?php
+            } ?>
+<p><?php echo $i_message['mysql_import_data']; ?></p>
+</div>
+<?php
+        } ?>
+<div class="center">
+	<form method="post" action="install.php?v=6">
+	<input type="button" class="submit" name="prev" value="<?php echo $i_message['install_prev']; ?>" onClick="history.go(-1)">&nbsp;
+	<input type="submit" class="submit" name="next" value="<?php echo $i_message['install_next']; ?>" <?php echo $allownext,$alert?>>
+	</form>
+</div>
+<?php
+    } elseif ($v == '6') {
+        $db_config = $_SESSION['config_file_content'];
+
+        mysql_connect($db_config['db_host'], $db_config['db_username'], $db_config['db_password']);
+        if (mysql_get_server_info() > '5.0') {
+            mysql_query("SET sql_mode = ''");
+        }
+        $db_config['db_charset'] = (strpos($db_config['db_charset'], '-') === false) ? $db_config['db_charset'] : str_replace('-', '', $db_config['db_charset']);
+        mysql_query("SET character_set_connection={$db_config['db_charset']}, character_set_results={$db_config['db_charset']}, character_set_client=binary");
+        mysql_select_db($db_config['db_name']);
+        $tablenum = 0;
+
+        $fp = fopen($installfile, 'rb');
+        $sql = fread($fp, filesize($installfile));
+        fclose($fp); ?>
+<div class="botBorder">
+<h4><?php echo $i_message['import_processing']; ?></h4>
+<div style="overflow-y:scroll;height:100px;width:715px;padding:5px;border:1px solid #ccc;">
+<?php
+    $db_charset = $db_config['db_charset'];
+        $db_prefix = $db_config['db_prefix'];
+        $sql = str_replace("\r", "\n", str_replace('`'.'ts_', '`'.$db_prefix, $sql));
+        foreach (explode(";\n", trim($sql)) as $query) {
+            $query = trim($query);
+            if ($query) {
+                if (substr($query, 0, 12) == 'CREATE TABLE') {
+                    $name = preg_replace('/CREATE TABLE ([A-Z ]*)`([a-z0-9_]+)` .*/is', '\\2', $query);
+                    echo '<p>'.$i_message['create_table'].' '.$name.' ... <span class="blue">OK</span></p>';
+                    @mysql_query(createtable($query, $db_charset));
+                    $tablenum++;
+                } else {
+                    @mysql_query($query);
+                }
+            }
+        } ?>
+</div>
+</div>
+<div class="botBorder">
+<h4><?php echo $i_message['create_founder']; ?></h4>
+
+<?php
+
+    if (intval($_SESSION['first_user_id']) > 0) {
+        $admin_id = intval($_SESSION['first_user_id']);
+        $sql0 = "ALTER TABLE `{$db_config['db_prefix']}user` AUTO_INCREMENT=".$admin_id.';';
+        if (mysql_query($sql0)) {
+            echo '<p>'.$i_message['set_auto_increment_success'].'... <span class="blue">OK..</span></p>';
+        } else {
+            echo '<p>'.$i_message['set_auto_increment_error'].'... <span class="red">ERROR</span></p>';
+            $admin_id = 1;
+        }
+    } else {
+        $admin_id = 1;
+    }
+
+    $siteFounder = $_SESSION['default_manager_account'];
+
+        $sql1 = 'INSERT INTO `%s` (`uid`, `password`, `login_salt`, `uname`, `email`, `phone`, `sex`, `location`, `is_audit`, `is_active`, `is_init`, `ctime`, `identity`, `api_key`, `domain`, `province`, `city`, `area`, `reg_ip`, `lang`, `timezone`, `is_del`, `first_letter`, `intro`, `last_login_time`, `last_feed_id`, `last_post_time`, `search_key`, `invite_code`, `feed_email_time`, `send_email_time`, `openid`, `input_city`, `is_fixed`) VALUES (%d, "%s", "11111", "管理员", "%s", NULL, 1, "北京 北京市 海淀区", 1, 1, 1, "%s", 1, NULL, "", 1, 2, 10, "127.0.0.1", "zh-cn", "PRC", 0, "G", "", "", 0, 0, "管理员 guanliyuan", NULL, 0, 0, NULL, NULL, 1);';
+        $sql1 = sprintf($sql1, $db_config['db_prefix'].'user', $admin_id, $siteFounder['password'], $siteFounder['email'], time());
+    // echo $sql1;
+    if (mysql_query($sql1)) {
+        echo '<p>'.$i_message['create_founderpower_success'].'... <span class="blue">OK</span></p>';
+    } else {
+        echo '<p>'.$i_message['create_founderpower_error'].'... <span class="red">ERROR</span></p>';
+        $quit = true;
+    }
+
+
+    $sql_user_group = "INSERT INTO `{$db_config['db_prefix']}user_group_link` (`id`,`uid`,`user_group_id`) VALUES ('1', ".$admin_id.",'1');";
+        if (mysql_query($sql_user_group)) {
+        } else {
+            $quit = true;
+        }
+
+        if (!$quit) {
+
+        file_put_contents(THINKSNS_ROOT.'/data/install.lock', 'ThinkSNS lock file');
+        } else {
+            echo 'please reinstall.';
+        } ?>
+</div>
+<div class="botBorder">
+<h4><?php echo $i_message['install_success']; ?></h4>
+<?php echo $i_message['install_success_intro']; ?>
+</div>
+<iframe src="<?php echo $_SESSION['site_url']; ?>/cleancache.php?all" height="0" width="0" style="display: none;"></iframe>
+<?php
+    }
+?>
+</div>
+<div class='copyright'>ThinkSNS <?php echo $_TSVERSION; ?> &#169; copyright 2008-<?php echo date('Y') ?> www.ThinkSNS.com All Rights Reserved</div>
+</div>
+<div style="display:none;">
+<script src="http://s79.cnzz.com/stat.php?id=1702264&web_id=1702264" language="JavaScript" charset="gb2312"></script>
+</div>
+</body>
+</html>
